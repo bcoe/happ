3a509e88f894b7fac915612455fc5f9f82d7e043
Benjamin Coe <bencoe@gmail.com>
2024-03-22 19:24:28 -0400

    feat: model data

---
+++ migrations/20240322223525_habits.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('habits', function (table) {
+        table.uuid('id', {primaryKey: true}).defaultTo(knex.fn.uuid());
+        table.uuid('user_id');
+        table.string('name', 1024).notNullable();
+        table.boolean('active').notNullable().defaultTo(true);
+        table.foreign('user_id').references('id').inTable('users');
+      }) 
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('habits');
+};
---
+++ migrations/20240322230145_habits_daily.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('habits_daily', function (table) {
+      table.uuid('habit_id');
+      table.date('date');
+      table.boolean('status').notNullable().defaultTo(false);
+      table.foreign('habit_id').references('id').inTable('habits');
+      table.primary(['habit_id', 'date']);
+      table.unique('habit_id');
+    }) 
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('habits_daily');
+};
