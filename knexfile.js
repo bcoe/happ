b387c39abc369a410ac81d0299ea42029f726588
Benjamin Coe <bencoe@gmail.com>
2024-04-03 17:15:35 -0400

    config: change config for heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
