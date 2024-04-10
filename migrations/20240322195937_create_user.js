90ec1d228de95e04cf04ea12d81d2669fb1b92ca
Benjamin E. Coe <benjamin.coe@sentry.io>
2024-04-10 10:39:21 -0400

    feat: bootstrap react application (#4)

--- migrations/20240322195937_create_user.js
+++ migrations/20240322195937_create_user.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
+export const up = function (knex) {
   return knex.schema
     .createTable('users', (table) => {
       table.uuid('id', { primaryKey: true }).defaultTo(knex.fn.uuid());
@@ -14,6 +14,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
+export const down = function (knex) {
   return knex.schema.dropTable('users');
 };
