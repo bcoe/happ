aa6ac098c1635bdb1562eccced72fced663fa6b7
Benjamin Coe <bencoe@gmail.com>
2024-04-11 11:45:23 -0400

    build: working on login flow

--- app/helpers/text-to-tailwind.tsx
+++
@@ -1,5 +0,0 @@
-import classes from '../style.module.css'
-
-export function ttt(text: string) {
-  return text.split(' ').map(text => classes[text]).join(' ');
-}
--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,20 @@
 import { withSentry, captureRemixErrorBoundaryError } from "@sentry/remix";
-import { useEffect, useState } from 'react';
 import {
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   useRouteError,
 } from "@remix-run/react";
-import { ttt } from './helpers/text-to-tailwind';
-import React, {useState} from 'react';
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-} from '@dnd-kit/core';
-import {
-  arrayMove,
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
+// import { ttt } from './helpers/text-to-tailwind';
+import type { LinksFunction } from "@remix-run/node";
+import styles from "./styles/shared.css?url";
 
-import {SortableItem} from './sortable-item';
-
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 function App() {
-  const [items, setItems] = useState([{id: 1, value: 'make coffee'}, {id: 2, value: 'brush my teeth'}, {id: 3, value: 'be rad'}, {id: 4, value: 'go climb a mountain'}]);
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(TouchSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
   return (
     <html>
       <head>
@@ -44,27 +22,17 @@ function App() {
           rel="icon"
           href="data:image/x-icon;base64,AA"
         />
-        <Meta />
         <Links />
+        <Meta />
       </head>
       <body>
-        <Outlet />
-        <div className={ttt('header')}/>
-        <div className={ttt('grid grid-cols-3')}>
+        <div className={'header'}>
+          <Link className={'font-medium text-blue-600 dark:text-blue-500 hover:underline'} to="/habits">Habits</Link>
+        </div>
+        <div className={'grid grid-cols-3'}>
           <div />
           <div>
-            <DndContext
-              sensors={sensors}
-              collisionDetection={closestCenter}
-              onDragEnd={handleDragEnd}
-            >
-                <SortableContext
-                  items={items}
-                  strategy={verticalListSortingStrategy}
-                >
-                  {items.map(item => <SortableItem value={item.value} key={item.id} id={item.id} />)}
-                </SortableContext>
-            </DndContext>
+            <Outlet />
           </div>
           <div />
         </div>
@@ -72,17 +40,6 @@ function App() {
       </body>
     </html>
   );
-
-  function handleDragEnd(event) {
-    const {active, over} = event;
-    if (active.id !== over.id) {
-      setItems((items) => {
-        const oldIndex = items.findIndex(item => item.id === active.id);
-        const newIndex = items.findIndex(item => item.id === over.id);
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
-  }
 }
 
 export const ErrorBoundary = () => {
