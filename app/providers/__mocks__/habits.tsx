98ab464c6c5f5d5b9ebb9c38846194e6aaca1d55
Benjamin Coe <bencoe@gmail.com>
2024-04-18 10:52:54 -0400

    build: fix knexfile

--- app/providers/__mocks__/habits.tsx
+++ app/providers/__mocks__/habits.tsx
@@ -19,19 +19,18 @@ export type Props = {
 };  
 
 interface HabitsType {
-    habits: Array<HabitType>; 
-    create: (type: string) => Promise<void>;
-    set: (habits: Array<HabitType>) => {};
-    remove: (prefix: string, created: number) => Promise<void>;
-  }
-  
-  const HabitsContext = React.createContext<HabitsType>(null!);
-  
-  export function HabitsProvider(props: Props) {
-    return <HabitsContext.Provider value={props.testValue}>{props.children}</HabitsContext.Provider>;
-  }
-  
-  export function useHabits() {
-    return React.useContext(HabitsContext);
-  }
-  
+  habits: Array<HabitType>;
+  create: (type: string) => Promise<void>;
+  set: (habits: Array<HabitType>) => {};
+  remove: (prefix: string, created: number) => Promise<void>;
+}
+
+const HabitsContext = React.createContext<HabitsType>(null!);
+
+export function HabitsProvider(props: Props) {
+  return <HabitsContext.Provider value={props.testValue}>{props.children}</HabitsContext.Provider>;
+}
+
+export function useHabits() {
+  return React.useContext(HabitsContext);
+}
